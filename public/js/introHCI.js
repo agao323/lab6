3a70a6bbde26c11104a76079f62f230634e5bee9
Allen Gao <agao323@gmail.com>
2017-02-16 10:35:17 -0800

    learning ajax

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,19 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, result);
 	console.log("User clicked on project " + idNumber);
+
+}
+
+function result(input) {
+	console.log(input);
+
+	var projectHTML = '<img class="detailsImage" src="' + input['image'] + '">' + '<p>' + input['title'] + '</p>' + '<p><small>' + input['date'] + '</small></p></a>' + '<p>' + input['summary'] + '</p>';
+
+	console.log(projectHTML);
+
+	$("#project" + input["id"]).find(".details").html(projectHTML);
 }
 
 /*
@@ -35,4 +47,21 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette/", colorFunc);
 }
+
+function colorFunc(input) {
+
+	var colors = input["colors"]["hex"];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+
+
+
+
